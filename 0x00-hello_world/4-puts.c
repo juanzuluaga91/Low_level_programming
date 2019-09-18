428093dc595b38f94d6833a757ad794f1d13afac
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-18 15:59:07 -0500

    Add 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h.>
-#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char string[40];
-strcpy(str, "Programming is like building a multilingual puzzle\n");
-puts(string);
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
