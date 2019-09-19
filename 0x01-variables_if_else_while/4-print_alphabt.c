87ff8e9ffab8f843b1512c8a958e11d57a3b8c8c
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-19 15:37:25 -0500

    Add 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: Write a program that prints the alphabet in
+ * lowercase, followed by a new line.
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+putchar ('\n');
+return (0);
+}
