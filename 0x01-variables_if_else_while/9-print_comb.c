69bbf367df29293c249b469aaf24f832434f704f
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-19 16:58:08 -0500

    Add 9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,15 @@
  */
 int main(void)
 {
-char ch, hc;
+int (ch);
 for (ch = '0' ; ch <= '9' ; ch++)
 {
 putchar(ch);
+if (ch < '9')
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar ('\n');
 return (0);
