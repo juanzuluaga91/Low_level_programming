327293641820d8dcd741a8b721625971f5f6bbaf
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-19 15:47:33 -0500

    Add5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * Return: (0) if is correct
+ * Description: Write a program that prints all single digit numbers of
+ * base 10 starting from 0, followed by a new line.
+ */
+int main(void)
+{
+char ch;
+for (ch = '0' ; ch <= '9' ; ch++)
+putchar(ch);
+putchar ('\n');
+return (0);
+}
