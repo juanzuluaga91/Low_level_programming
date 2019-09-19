fc8a5f8f307bc2d882f9039e37976f0abc0677b2
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-18 22:23:10 -0500

    Add 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Accept empty value
+ * printf - Prints inputs
+ * sizeof - Show the size
+ * @void: Description of a empty parameter
  *
- * Return: Always 0 (Success)
+ * Description: This is going to print
+ * Return: 0 Value if it works
  */
 int main(void)
 {
-printf("size of a char: %d byte(s)\n", (int)sizeof(char));
-printf("size of a int: %d byte(s)\n", (int)sizeof(int));
-printf("size of a long int: %d byte(s)\n", (int)sizeof(long int));
-printf("size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
-printf("size of a float: %d byte(s)\n", (int)sizeof(float));
+printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
 return (0);
 }
