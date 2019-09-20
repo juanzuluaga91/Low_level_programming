809d20a28ff3baca59d6d735e672fe2ccf2ec1d6
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-19 22:39:11 -0500

    Add 10-print_comb2.c file

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -2,22 +2,25 @@
 /**
  * main - Accept empty value
  * Return: (0) if is correct
- * Description: Write a program that prints all the numbers of base 16
+ * Description: Write a program that prints the numbers from 00 to 99.
  * in lowercase, followed by a new line.
  */
 int main(void)
 {
 int ch, hc;
-for (ch = '0' ; ch <= '9' ; ch++)
+for (ch = '0'; ch <= '9'; ch++)
 {
-for (hc = '0' ; hc <= '9' ; hc++)
+for (hc = '0'; hc <= '9'; hc++)
 {
 putchar(ch);
 putchar(hc);
+if (!(ch == '9' && hc == '9'))
+{
 putchar(',');
 putchar(' ');
 }
 }
-putchar ('\n');
+}
+putchar('\n');
 return (0);
 }
