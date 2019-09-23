c01aba556e9caaed53024648444b71f6f3564592
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-23 09:44:31 -0500

    first commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * Return: (0) if is correct
+ * Description: Write a program that prints Holberton followed by a new line.
+ */
+int main(void)
+{
+printf("Holberton\n");
+return (0);
+}
