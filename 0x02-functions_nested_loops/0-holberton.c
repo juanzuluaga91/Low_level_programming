d574a02b7ecf29e0d35d78a3c0ac10f92126a14d
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-23 19:52:34 -0500

    Adding 0-holberton.c file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- * main - Accept empty value
- * Return: (0) if is correct
- * Description: Write a program that prints Holberton followed by a new line.
+ * main - Write a program that prints Holberton, followed by a new line.
+ *
+ * Return: (0);
  */
 int main(void)
 {
-printf("Holberton\n");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
