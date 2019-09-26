040a45175f102ded11bd492cd4342837e3ec1f0f
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-26 16:24:17 -0500

    Add 4-print_most_numbers.c file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
-  
-#include "holberton.h"
+ #include "holberton.h"
 /**
  * print_most_numbers - print numbers 0..9\n
  *
@@ -9,16 +8,15 @@
  */
 void print_most_numbers(void)
 {
-	int i = '0';
-
-	while (i <= '9')
-	{
-		if (i == '2' || i == '4')
-		{
-			i++;
-			continue;
-		}
-		_putchar(i++);
-	}
-	_putchar('\n');
+int i = '0';
+while (i <= '9')
+{
+if (i == '2' || i == '4')
+{
+i++;
+continue;
+}
+_putchar(i++);
+}
+_putchar('\n');
 }
