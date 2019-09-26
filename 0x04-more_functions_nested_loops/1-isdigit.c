e918bf94bafc18668e3578ce067b5887a841be4a
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-26 15:00:28 -0500

    Add 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks if int is digit or not
+ *
+ * @c: int c, represents a char
+ *
+ * Return: 1 if c is digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
