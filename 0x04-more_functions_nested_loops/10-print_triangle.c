05216d78bae9029cb55616865071317ca8f9ed28
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-09-26 22:15:45 -0500

    Fix error function prototype

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,28 +2,28 @@
 /**
  * print_triangle - prints a triangle
  *
- * @tri: size x size right triangle
+ * @size: sizexsize right triangle
  *
  * Return: always void
  */
-void print_triangle(int tri)
+void print_triangle(int size)
 {
-	int i, n, tri;
+	int i, n, spaceC;
 
-	if (tri < 1)
+	if (size < 1)
 	{
 		_putchar('\n');
 		return;
 	}
 	i = 0;
-	tri = tri - 1;
-	while (i < tri)
+	spaceC = size - 1;
+	while (i < size)
 	{
 		n = 0;
-		while (n < tri)
-			_putchar((n++ < tri) ? ' ' : '#');
+		while (n < size)
+			_putchar((n++ < spaceC) ? ' ' : '#');
 		_putchar('\n');
-		tri--;
+		spaceC--;
 		i++;
 	}
 }
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -8,5 +8,5 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int sqr);
-void print_triangle(int tri);
+void print_triangle(int size);
 void print_number(int n);
