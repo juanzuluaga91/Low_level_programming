a59e01de4c903577e2a937206162b4859cf1f8e7
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-10-01 14:18:40 -0500

    Changing the for loop to n results

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,14 +1,22 @@
 #include "holberton.h"
+
 /**
- * puts_half - Print half o a string
- * @str: string of formula
+ * puts_half - print second half of a string
+ * @str: char array string type
+ * Description: If odd number of chars, print (length - 1) / 2
  */
+
 void puts_half(char *str)
 {
-int i;
-for (i = 5 ; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
