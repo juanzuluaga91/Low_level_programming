028467c673197accaae8057ee920bbf999478966
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-10-01 13:59:26 -0500

    Fix variable and add a loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,18 +2,19 @@
 #include "holberton.h"
 
 /**
- * rev_string - reverse a string
- * @i: length of str aaray.
+ * puts2 - Prints out every other char of the string.
+ * @str: input string to print.
  */
 
 void puts2(char *str)
 {
-    int i;
-    
-	for(i = 0 ; str[i] != '\0'; i++)
-    {
-        if (i % 2 == 0)
-            _putchar(str[i]);
-    }
-    _putchar('\n');
+	int ch;
+
+	while (str[ch] != '\0')
+	{
+		if (ch % 2 == 0)
+			_putchar(str[ch]);
+		ch++;
+	}
+	_putchar('\n');
 }
