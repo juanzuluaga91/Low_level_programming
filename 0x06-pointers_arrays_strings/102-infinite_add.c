a6c22beb496611922091bfba793082697c9f2fc6
Juan Pablo <juanzuluaga91@gmail.com>
2019-10-03 18:46:38 -0500

    Fix description

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,73 +1,55 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
- *infinite_add - adds two numbers.
- *@n1: first number to be summed
- *@n2: second number to be summed
- *@r: output buffer
- *@size_r: buffer size
- *Return: pointer to output
+ * infinite_add - adds two strings
+ * @n1: string 1
+ * @n2: string 2
+ * @r: added string
+ * @size_r: buffer of string
+ * Return: r
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
-	int control;
-	int cont = 0;
-	int tmpCont = 0;
-	unsigned long long int tmp = 1;
-	unsigned long long int num1 = 0;
-	unsigned long long int num2 = 0;
-	unsigned long long int tmpResult;
+	int num, i, j, k, l, m, sum, carry, digit1, digit2;
 
-	while (*(n1 + i))
-	{
+	i = l = j = k = carry =  0;
+	while (n1[i] != '\0')
 		i++;
-	}
-	while (cont != i)
-	{
-		tmpCont = 0;
-		while (tmpCont != cont)
-		{
-			tmp *= 10;
-			tmpCont++;
-		}
-		num1 += *(n1 + (i - cont)) * tmp;
-		cont++;
-	}
-
-	i = 0;
-	while (*(n2 + i))
-	{
-		num2 += *(n2 + i) + '0';
-		i++;
-	}
-
-	printf("%lli | %lli\n", num1, num2);
-	tmpResult = num1 + num2;
-	printf("%lli", tmpResult);
-
-
-	i = 0;
-	control = tmpResult;
-	while (control != 0 )
-	{
-		control /= 10;
-		i++;
-	}
-
-	if (i == size_r)
-	{
-		while (i != 0)
-		{
-			*(r + i) = tmpResult % 10;
-			tmpResult /= 10;
-			i--;
-		}
-		return (r);
-	}
-	else
-	{
+	while (n2[j] != '\0')
+		j++;
+	if (i + 2 > size_r || j + 2 > size_r)
 		return (0);
+	i = i - 1;
+	j = j - 1;
+	while (i >= 0 || j >= 0)
+	{
+		digit1 = digit2 = 0;
+		if (i >= 0)
+			digit1 = n1[i--] - '0';
+		if (j >= 0)
+			digit2 = n2[j--] - '0';
+		sum = digit1 + digit2 + carry;
+		if (sum > 9)
+		{
+			carry = 1;
+			sum = sum - 10;
+		}
+		else
+			carry = 0;
+		r[k++] = (sum + '0');
 	}
+	if (carry == 1)
+		r[k++] = (1 + '0');
+	m = k;
+	k = k - 1;
+	for (l = 0; l < k; l++, k--)
+	{
+		num = r[k];
+		r[k] = r[l];
+		r[l] = num;
+	}
+	r[m] = '\0';
+	return (r);
 }
