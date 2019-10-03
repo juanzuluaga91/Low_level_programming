b541d0b7ef5ec5ed53dabb606107cf54a5d017cd
Juan Pablo <juanpablo@Juans-MacBook.local>
2019-10-03 14:47:43 -0500

    Add 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - compares two strings.
+ *@s1: string one
+ *@s2: string two
+ *Return: int if zero s1 = s2, if neg s1 < s2 and if pos s2 < s1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x1 = 0;
+	int x2 = 0;
+	int z = 0;
+
+	while (z == 0 && (*(s1 + x1) || *(s2 + x2)))
+	{
+		z = *(s1 + x1) - *(s2 + x2);
+		x1++;
+		x2++;
+	}
+	return (z);
+}
