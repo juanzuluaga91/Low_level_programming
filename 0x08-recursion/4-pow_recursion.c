c33eeee69a5e57d498a524701c4d1a1039d07aef
Juan Pablo <juanzuluaga91@gmail.com>
2019-10-09 13:03:02 -0500

    fix header

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,16 @@
-#include "holberton.h"
+#include"holberton.h"
 /**
- * _pow_recursion - returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y.
  * @x: base number
- * @y  power of
- * Return: Result of x raised to the power of y.
+ * @y: power of
+ * Return: x to the power of y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-    else if (y <= 0)
-    {
-        return (1);
-    }
-    
-	return (x *_pow_recursion(x, y-1));
+	else if (y <= 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
