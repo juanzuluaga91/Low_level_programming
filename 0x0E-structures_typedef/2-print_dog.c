6416ef7c4e77c1f25efb7913b25e6298f86546b0
Juan Pablo <juanzuluaga91@gmail.com>
2019-10-21 21:13:40 -0500

    Correcting ptr print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,11 @@ void print_dog(struct dog *d)
 		if ((*d).name != NULL)
 			printf("Name: %s\n", (*d).name);
 		else
-			printf("Name: %s\n", (*d).name);
+			printf("Name: %p\n", (*d).name);
 		printf("Age: %.6f\n", (*d).age);
 		if ((*d).owner != NULL)
 			printf("Owner: %s\n", (*d).owner);
 		else
-			printf("Owner: %s\n", (*d).owner);
+			printf("Owner: %p\n", (*d).owner);
 	}
 }
