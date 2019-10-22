c4a8e9a41fbbc0266788bf4a2082cb499dda60ea
Juan Pablo <juanzuluaga91@gmail.com>
2019-10-21 20:41:07 -0500

    Fix heaader

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG
+#define DOG
+/**
+ *struct dog - structure for a dog
+ *@name: name
+ *@age: age
+ *@owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
+
+#ifndef _DOG_
+#define _DOG_
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
