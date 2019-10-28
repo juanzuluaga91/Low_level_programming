b279c279a4b2ee29fb6abbef89917fe8056d18fe
Juan  Zuluaga Pizza <juanzuluagapizza@MacBook-de-Juan.local>
2019-10-27 23:23:40 -0500

    Fix Va_start Wrong

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,27 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+
 /**
-  * sum_them_all - sums all arguments
-  * @n: number of arguments
-  * @...: variable list of arguments
-  * Return: sum of all arguments
-  */
+ * sum_them_all - variadic function to add number
+ * @n: const unsigned int type
+ * Return: sum of numbers
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, result;
-	va_list ap;
+	unsigned int sum = 0;
+	unsigned int i;
+	va_list alist;
+
+	va_start(alist, n);
 
 	if (n == 0)
 		return (0);
-	va_start(ap, n);
-	for (i = result = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		result += va_arg(ap, int);
+		sum += va_arg(alist, unsigned int);
 	}
-	va_end(ap);
-	return (result);
+	va_end(alist);
+
+	return (sum);
 }
