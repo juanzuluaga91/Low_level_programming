67452c146f760078e108512672f1694a9855002b
juanzuluaga91 <juanzuluagapizza@MacBook-de-Juan.local>
2019-11-12 22:18:35 -0500

    [Fix] Correcting betty style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,34 +7,34 @@
   */
 int main(int ac, char *av[])
 {
-	int fd_from, fd_to, rd_stat, wr_stat;
+	int ffrom, fto, rstar, wstar;
 	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 	char buffer[1024];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
+	ffrom = open(av[1], O_RDONLY);
+	if (ffrom == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
-	if (fd_to == -1)
+	fto = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	if (fto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	rd_stat = 1;
-	while (rd_stat)
+	rstar = 1;
+	while (rstar)
 	{
-		rd_stat = read(fd_from, buffer, 1024);
-		if (rd_stat == -1)
+		rstar = read(ffrom, buffer, 1024);
+		if (rstar == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		if (rd_stat > 0)
+		if (rstar > 0)
 		{
-			wr_stat = write(fd_to, buffer, rd_stat);
-			if (wr_stat != rd_stat || wr_stat == -1)
+			wstar = write(fto, buffer, rstar);
+			if (wstar != rstar || wstar == -1)
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
-	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	if (close(ffrom) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom), exit(100);
+	if (close(fto) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto), exit(100);
 	return (0);
 }
