989ac19fd9722fa92ea0b782b583031b2a054efa
juanzuluaga91 <juanzuluagapizza@MacBook-de-Juan.local>
2019-11-12 22:20:55 -0500

    [Fix] Betty style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,26 +8,26 @@
 int main(int ac, char *av[])
 {
 	int ffrom, fto, rstar, wstar;
-	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
-	char buffer[1024];
+	mode_t p_v = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	char buf[1024];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	ffrom = open(av[1], O_RDONLY);
 	if (ffrom == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fto = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	fto = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, p_v);
 	if (fto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	rstar = 1;
 	while (rstar)
 	{
-		rstar = read(ffrom, buffer, 1024);
+		rstar = read(ffrom, buf, 1024);
 		if (rstar == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		if (rstar > 0)
 		{
-			wstar = write(fto, buffer, rstar);
+			wstar = write(fto, buf, rstar);
 			if (wstar != rstar || wstar == -1)
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
